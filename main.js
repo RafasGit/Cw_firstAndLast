93752dba55c8e01edd4487ef03ca2460fa3cb4c7
Josh Ng'ang'a <raphaelnganga4@gmail.com>
2023-07-24 23:44:26 +0300

    final js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//We remove the first and last characters from a given string using the shift and pop method after splitting the text into array elements
+//We remove the first and last characters from a given string using the shift and pop method after splitting the text into array elements as strings dont have native shift and pop methods
 function removeChar(str){
     //You got this!
     let str1 = str.split('');
